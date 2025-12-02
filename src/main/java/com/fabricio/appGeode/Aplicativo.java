ccc3f632eb4b42cf2d2e7784832d67c983b39ab3
estudante668 <desenhista668@gmail.com>
2025-12-01 21:55:08 -0300

    API-Acesso ao Geode Apache

---
+++ src/main/java/com/fabricio/appGeode/Aplicativo.java
@@ -0,0 +1,26 @@
+package com.fabricio.appGeode;
+
+import org.apache.geode.cache.client.ClientRegionShortcut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.gemfire.config.annotation.ClientCacheApplication;
+import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
+import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
+
+import com.fabricio.appGeode.modelo.Pessoa;
+
+@SpringBootApplication
+@ClientCacheApplication(name = "AccessandoGemFireDataRestApplication")
+@EnableEntityDefinedRegions(
+  basePackageClasses = Pessoa.class,
+  clientRegionShortcut = ClientRegionShortcut.LOCAL
+)
+@EnableGemfireRepositories
+@SuppressWarnings("unused")
+public class Aplicativo {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Aplicativo.class, args);
+  }
+}
---
+++ src/main/java/com/fabricio/appGeode/repositorio/PessoaRepositorio.java
@@ -0,0 +1,16 @@
+package com.fabricio.appGeode.repositorio;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import com.fabricio.appGeode.modelo.Pessoa;
+
+@RepositoryRestResource(collectionResourceRel = "pessoa", path = "pessoa")
+
+public interface PessoaRepositorio extends CrudRepository<Pessoa, Long> {
+
+  List<Pessoa> findByLastName(@Param("nome") String nome);
+
+}
